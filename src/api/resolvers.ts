5d610c0e0b2f7816015a87b45cfd8409ef37d88d
Tomasz <tomaszporeba1@gmail.com>
2020-12-12 12:58:16 +0100

    com

--- src/api/resolvers.ts
+++ src/api/resolvers.ts
@@ -3,6 +3,7 @@ import {User} from "../../models/users";
 export const resolvers = {
     Query: {
         async users() {
+
             const users = await User.findAll();
             return users.map(user => ({
                 id: user.dataValues.id,
